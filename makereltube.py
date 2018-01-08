74731e15424cd5c93c95e64fbbfa352f2f4e75b4
Adrian Nettleship <info@adriannettleship.com>
2018-01-08 22:43:09 +0000

    Working, but need to make letters unique before searching

--- makereltube.py
+++ makereltube.py
@@ -5,29 +5,54 @@
 #
 #
 
-f = open('dictionary.txt', 'r')
+f = open('words.txt', 'r')
 dictionary = f.read().splitlines()
 
 f = open('wordSet.txt', 'r')
 wordSet = f.read().splitlines()
+
+results = []
 
 for dictWord in dictionary:
     print("Testing:")
     print(dictWord)
     letters = list(dictWord)
     
-    remainingWords = wordSet
+    def checkLetters(dictWord,wordSet):
+        """
+        How many words in the array wordSet contain none of the letters in the dictWord parameter?
+        """
+        print('Checking '+dictWord)
+        
+        wordSetNotMatches = []
+        
+        for word in wordSet:
+            matches = 0
+            print(word)
+            for letter in dictWord:
+                if word.lower().count(letter.lower()) > 0:
+                    print (letter+' is in '+word)
+                    matches += 1
+                else:
+                    print (letter+' is not in '+word)
+            print('Number of matches of '+dictWord+' in '+word+': '+str(matches))
+            if matches == 0:
+                wordSetNotMatches.append(word)
+            
+        print('Checked '+dictWord+'. Words in word set that contain no letters from '+dictWord+': ')
+        print(len(wordSetNotMatches))
+        return wordSetNotMatches
+            
     
-    for letter in letters:
-        print(letter)
-
-        # Take second list array. For each letter in the dictionary word, remove words from the second list that contain that letter.
-        
-        # If array contains only one entry, we have a winner.
-        
-        remainingWords = filter(lambda listWord: letter not in listWord, remainingWords)
-        
-        print(remainingWords)
-        print(len(remainingWords))
+    notMatches = checkLetters(dictWord,wordSet)
+    print('')
+    if len(notMatches) == 1:
+        print('Found exactly one word in word set that contains no letters from the word '+dictWord)
+        results.append(dictWord)
+    else:
+        print('Found '+str(len(notMatches))+' words in word set that contain no letters from: '+dictWord)
+    print('')
 
 print("")
+print("Words whose letters only appear in one memeber of the word set:")
+print(results)
